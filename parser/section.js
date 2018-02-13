6df1e89c7a8358bd0fc3847a4502c3b838ec5627
António Borba da Silva <abs@ggl.pt>
2018-02-13 11:18:58 +0000

    section to class structure

--- parser/section.js
+++ parser/section.js
@@ -22,30 +22,35 @@ numberTypes['Seccao'] = tools.isRoman
 numberTypes['Subseccao'] = tools.isRoman
 numberTypes['Artigo'] = tools.isNumeral
 
-function Section (textLine, layer) {
-	if (! textLine || typeof textLine !== 'string' || textLine.isEmpty()) throw new Error('Section: textLine not supplied')
-	if (! layer || typeof layer !== 'string' || layer.isEmpty()) throw new Error('Section: layer not supplied')
-    this.layer = layer.clean()
-	textLine = textLine.clean()
-    var parts = textLine.split(' ', 2)
-    if (parts.length < 2) throw new Error('Section/' + this.layer + ': ' + 'Invalid parameter textLine')
-	this.type = parts[0]
-    this.nr = parts[1]
-	this.separator = ''
-	var remain = textLine.slice(parts[0].concat(' ').concat(parts[1]).concat(' ').length)
-    if (remain.length === 0) return
-	this.epigraph = remain
-	this.inner = []
-}
-Section.prototype.add = function(obj) {
-	if (!obj) return
-	if (typeof obj === 'string') {
-		obj = obj.clean()
-		if (obj.isEmpty()) return
-		this.epigraph = obj
-		return
+class Section {
+
+	constructor(textLine, layer) {
+		if (! textLine || typeof textLine !== 'string' || textLine.isEmpty()) throw new Error('Section: textLine not supplied')
+		if (! layer || typeof layer !== 'string' || layer.isEmpty()) throw new Error('Section: layer not supplied')
+		this.layer = layer.clean()
+		textLine = textLine.clean()
+		var parts = textLine.split(' ', 2)
+		if (parts.length < 2) throw new Error('Section/' + this.layer + ': ' + 'Invalid parameter textLine')
+		this.type = parts[0]
+		this.nr = parts[1]
+		this.separator = ''
+		var remain = textLine.slice(parts[0].concat(' ').concat(parts[1]).concat(' ').length)
+		if (remain.length === 0) return
+		this.epigraph = remain
+		this.inner = []
 	}
-	this.inner.push(obj)
+
+	add(obj) {
+		if (!obj) return
+		if (typeof obj === 'string') {
+			obj = obj.clean()
+			if (obj.isEmpty()) return
+			this.epigraph = obj
+			return
+		}
+		this.inner.push(obj)
+	}
+	
 }
 
 function sectionType (value) {
