7c0e4580ddfbbb1e3a5900496448e6b907ce0e53
António Borba da Silva <abs@ggl.pt>
2018-02-15 20:58:26 +0000

    tests refactoring

--- test/parser/dispatcher-test.js
+++ test/parser/dispatcher-test.js
@@ -1,64 +1,49 @@
 var assert = require('assert')
-var Dispatcher = require('../../parser/dispatcher');
+var Dispatcher = require('../../parser/dispatcher')
 var sampleText = require('../../sampleText')
+var Book = require('../../parser/book'),
+	Part = require('../../parser/part'),
+	Title = require('../../parser/title'),
+	Chapter = require('../../parser/chapter'),
+	Section = require('../../parser/section'),
+	Subsection = require('../../parser/subsection'),
+	Article = require('../../parser/article'),
+	Li_1 = require('../../parser/li_1'),
+	Li_a = require('../../parser/li_a'),
+	Li_i = require('../../parser/li_i')
 
 // DISPATCHER PARAMETERS
 
-var docOrdering = ['book','part','title','chapter','section','subsection','article']
+var sectionsVector = ['book','part','title','chapter','section','subsection','article']
 
-var articlesOrdering = { layers:['li_1','li_a','li_i'], comments:'comments' }
-
-var matchCheckers = []
-matchCheckers['book'] = (value) => {return isOfType(value, "livro", isRoman) }
-matchCheckers['part'] = (value) => {return isOfType(value, "parte", isRoman) }
-matchCheckers['title'] = (value) => {return isOfType(value, ["titulo", "título"], isRoman) }
-matchCheckers['chapter'] = (value) => {return isOfType(value, ["capitulo", "capítulo"], isRoman) }
-matchCheckers['section'] = (value) => {return isOfType(value, ["seccao", "secção"], isRoman) }
-matchCheckers['subsection'] = (value) => {return isOfType(value, ["subseccao", "sub-seccao", "subsecção", "sub-secção"], isRoman) }
-matchCheckers['article'] = (value) => {return isOfType(value, "artigo", isNumeral) }
-matchCheckers['li_1'] = (value) => {
-	return ((inArticleStatus && lastTag === 'article') || /^\d+/.test(value.clean()))
-}
-matchCheckers['li_a'] = (value, inArticleStatus, lastTag, status) => {
-	if (! (inArticleStatus && status && lastTag)) return false
-	var val = value.clean()
-	if (! /^[a-z]+.?\s.*/.test(val)) return false
-	var word = val.replace(/(\w+)(.*)/i, '$1').toLowerCase()
-	if (word === 'a' && status !== 'li_1') return false
-	if (isRoman(word) && status === 'li_a' && ! ['h', 'u', 'v', 'k', 'b', 'c', 'l'].contains(lastTag)) return false
-	if (isRoman(word) && status === 'li_i' && roman2arabic(word) === roman2arabic(lastTag) + 1) return false
-	status = 'li'
-	return true
-}
-matchCheckers['li_i'] = (value, inArticleStatus, lastTag) => {
-	var val = value.clean()
-	var word = val.replace(/(\w+)(.*)/i, '$1').toLowerCase()
-	if (roman2arabic(word) === roman2arabic(lastTag) + 1) return 'LiI'
-	var tmp = (/^(?:[IVXLCDM])+\.\s.*/i).test(val)
-	return (status !== 'Comments' && (/^(?:[IVXLCDM])+\.\s.*/i).test(val))
-}
-matchCheckers['comments'] = (value, inArticleStatus, lastTag) => {
-	return ['li_1','li_a','li_i', 'comments'].contains(status)
-}
+var articlesStructure = { vector:['li_1','li_a','li_i'], comments:'comments' }
 
 var workflow = []
-workflow['start']      = ['article','book','part','title','chapter','section']
-workflow['book']       = ['article','part','title','chapter','section']
-workflow['part']       = ['article','title','chapter','section']
-workflow['title']      = ['article','chapter','section']
-workflow['chapter']    = ['article','section']
-workflow['section']    = ['article','subsection']
-workflow['subsection'] = ['article']
-workflow['article']    = ['article','book','part','title','chapter','section','subsection']
+	workflow['start']      = ['book','part','title','chapter','section','article']
+	workflow['book']       = ['part','title','chapter','section','article']
+	workflow['part']       = ['title','chapter','section','article']
+	workflow['title']      = ['chapter','section','article']
+	workflow['chapter']    = ['section','article']
+	workflow['section']    = ['subsection','article']
+	workflow['subsection'] = ['article']
+	workflow['article']    = ['book','part','title','chapter','section','subsection','article']
 
+var classesMap = {
+	book:Book, part:Part, title:Title, chapter:Chapter, section:Section, subsection:Subsection, article:Article,
+	li_1:Li_1, li_a:Li_a, li_i:Li_i
+}
+	
 describe('Dispatcher', function() {
     
-    var dispatcher = new Dispatcher(docOrdering, articlesOrdering, matchCheckers, workflow)
-    dispatcher.parse(sampleText.text)
-    
-    describe('#Dispatcher(docOrdering, articlesOrdering, matchCheckers, workflow)', function() {
+	var dispatcher = new Dispatcher(sectionsVector, articlesStructure, workflow, classesMap)
+	var parseLine = dispatcher.parseLine.bind(dispatcher)
+	for (var line of sampleText.text) parseLine(line)
+	var getJSON = dispatcher.getJSON.bind(dispatcher)
+	var json = getJSON()
+
+    describe('#Dispatcher(sectionsVector, articlesStructure, matchCheckers, workflow)', function() {
         it('should parse text corretly into objects', function() {
-            assert.equal(true, true)
+            assert.equal(json, '[{"type":"part","header":"Parte","nr":"I","separator":"","epigraph":"Âmbito de aplicação"}]')
         })
     })
     
