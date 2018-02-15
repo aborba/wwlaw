a84e0e8772a7afe7922015f2bab62f33be14d5ad
António Borba da Silva <abs@ggl.pt>
2018-02-15 19:39:33 +0000

    article tests refactoring

--- test/parser/article-test.js
+++ test/parser/article-test.js
@@ -1,75 +1,73 @@
 var assert = require('assert')
-var Article = require('../../parser/article');
+var Article = require('../../parser/article')
 
 describe('Article', function() {
     
-    var artigo = new Article('Artigo 1.º', 'Artigo')
+    var article = new Article('Artigo 1.º')
     
-    describe('#Artigo("Artigo 1.º", "Artigo")', function() {
+    describe('#Article("Artigo 1.º")', function() {
         it('should return values for "Artigo 1.º" and "undefined" for epigraph', function() {
-            assert.equal(artigo.layer, 'Artigo')
-            assert.equal(artigo.type, 'Artigo')
-            assert.equal(artigo.nr, '1.º')
-            assert.equal(typeof artigo.epigraph, 'undefined')
+            assert.equal(article.type, 'article')
+            assert.equal(article.header, 'Artigo')
+            assert.equal(article.nr, '1.º')
+            assert.equal(typeof article.epigraph, 'undefined')
         })
     })
     
-    var artigo2 = new Article('Artigo I Epígrafe do artigo 1.º', 'Artigo')
+    var article2 = new Article('Artigo 1.º Epígrafe do artigo 1.º')
     
-    describe('#Artigo("Artigo I Epígrafe do artigo 1.º", "Artigo")', function() {
-        it('should return values for "Artigo I Epígrafe do artigo 1.º"', function() {
-            assert.equal(artigo2.layer, 'Artigo')
-            assert.equal(artigo2.type, 'Artigo')
-            assert.equal(artigo2.nr, 'I')
-            assert.equal(artigo2.epigraph, 'Epígrafe do artigo 1.º')
+    describe('#Artigo("Artigo 1.º Epígrafe do artigo 1.º")', function() {
+        it('should return values for "Artigo 1.º Epígrafe do artigo 1.º"', function() {
+            assert.equal(article2.type, 'article')
+            assert.equal(article2.header, 'Artigo')
+            assert.equal(article2.nr, '1.º')
+            assert.equal(article2.epigraph, 'Epígrafe do artigo 1.º')
         })
     })
     
-    var artigo3 = new Article('Artigo I', 'Artigo')
-    artigo3.add('Epígrafe do artigo 1.º')
+    var article3 = new Article('Artigo 1.º')
+    article3.add('Epígrafe do artigo 1.º')
     
-    describe('#Artigo("Artigo I", "Artigo").add("Epígrafe do artigo 1.º")', function() {
-        it('should return values for "Artigo I Epígrafe do artigo 1.º"', function() {
-            assert.equal(artigo3.layer, 'Artigo')
-            assert.equal(artigo3.type, 'Artigo')
-            assert.equal(artigo3.nr, 'I')
-            assert.equal(artigo3.epigraph, 'Epígrafe do artigo 1.º')
+    describe('#Article("Artigo 1.º").add("Epígrafe do artigo 1.º")', function() {
+        it('should return values for "Artigo 1.º Epígrafe do artigo 1.º"', function() {
+            assert.equal(article3.type, 'article')
+            assert.equal(article3.header, 'Artigo')
+            assert.equal(article3.nr, '1.º')
+            assert.equal(article3.epigraph, 'Epígrafe do artigo 1.º')
         })
     })
 
-    var artigo4 = new Article('Artigo I', 'Artigo')
-    artigo4.add('Epígrafe do artigo 1.º')
-    artigo4.add('comentário')
+    var article4 = new Article('Artigo 1.º')
+    article4.add('Epígrafe do artigo 1.º')
+    article4.add('comentário')
     
-    describe('#Artigo("Artigo I", "Artigo").add("Epígrafe do artigo 1.º").add("comentário")', function() {
+    describe('#Artigo("Artigo 1.º").add("Epígrafe do artigo 1.º").add("comentário")', function() {
         it('should return values for "comentário"', function() {
-            assert.equal(artigo4.layer, 'Artigo')
-            assert.equal(artigo4.type, 'Artigo')
-            assert.equal(artigo4.nr, 'I')
-            assert.equal(artigo4.epigraph, 'Epígrafe do artigo 1.º')
-            assert.equal(artigo4.comments.length, 1)
-            assert.equal(artigo4.comments[0], 'comentário')
+            assert.equal(article4.type, 'article')
+            assert.equal(article4.header, 'Artigo')
+            assert.equal(article4.nr, '1.º')
+            assert.equal(article4.epigraph, 'Epígrafe do artigo 1.º')
+            assert.equal(article4.comments.length, 1)
+            assert.equal(article4.comments[0], 'comentário')
         })
     })
 
-    
-    /*
-    describe('#Parte.is("Parte I")', function() {
-        it('should return true for "Parte I"', function() {
-            assert.equal(Parte.is('Parte I'), true)
+    describe('#Article.matches("Artigo 1.º")', function() {
+        it('should return true for "Artigo 1.º"', function() {
+            assert.equal(Article.matches('Artigo 1.º'), true)
         })
     })
     
-    describe('#Parte.is(" paRte ii ")', function() {
-        it('should return true for " paRte ii "', function() {
-            assert.equal(Parte.is(' paRte ii '), true)
+    describe('#Article.matches(" arTigo 12.º-A ")', function() {
+        it('should return true for " arTigo 12.º-A "', function() {
+            assert.equal(Article.matches(' arTigo 12.º-A '), true)
         })
     })
     
-    describe('#Parte.is("Parte 1")', function() {
+    describe('#Article.matches("Parte 1")', function() {
         it('should return false for "Parte 1"', function() {
-            assert.equal(Parte.is('Parte 1'), false)
+            assert.equal(Article.matches('Parte 1'), false)
         })
     })
-    */
+
 })
